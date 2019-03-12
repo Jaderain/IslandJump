bad4cfe6d47d8324182ef72951cc78202f2523cb
Jaderain <brandon.minwoo.lee@gmail.com>
2019-03-12 00:40:39 -0400

    Init Commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject islandObject;
+    public Vector3[] islandSpawns;
+
+    // Use this for initialization
+    void Start() {
+        initPlatforms();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void initPlatforms()
+    {
+        foreach (Vector3 item in islandSpawns)
+        {
+            Instantiate(islandObject, item, Quaternion.Euler(new Vector3(0, 45, 0)));
+        }
+    }
+}
